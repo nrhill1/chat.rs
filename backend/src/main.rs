4127ebe27f2ddf2f302e94425e45d6229806c006
nic <nrhill1@gmail.com>
2024-02-24 13:38:05 -0800

    socketio and socketioxide working

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::Method, routing::get};
+use socketioxide::{
+    extract::SocketRef,
+    SocketIo,
+};
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (layer, io) = SocketIo::new_layer();
+
+    // Register a handler for the default namespace
+    io.ns("/", |s: SocketRef| {
+        // For each "message" event received, send a "message-back" event with the "Hello World!" event
+        s.on("message", |s: SocketRef| {
+            s.emit("message-back", "Hello World!").ok();
+        });
+    });
+
+    let cors = CorsLayer::new()
+        // allow `GET` and `POST` when accessing the resource
+        .allow_methods([Method::GET, Method::POST])
+        // allow requests from any origin
+        .allow_origin(Any);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(layer)
+        .layer(cors);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,35 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import { io, Socket } from 'socket.io-client';
 
 export default function Home() {
+
+  const socket = io('ws://0.0.0.0:8080/');
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.on('connect', () => {
+      console.log('socket connected!');
+    });
+
+    socket.on('message-back', (data: any) => {
+      console.log('message back: ', data);
+      setMessage(data);
+    });
+  });
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      </div>
-    </main>
-  );
-}
+    <div>
+      <h1>Home</h1>
+      <p>{message}</p>
+      <button onClick={() => socket.send('message', 'Hello')}>Send</button>
+    </div>
+  )
+
+};
+
