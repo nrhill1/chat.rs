8e22f4cd3e0a6b455e80a8b9985dbea1b7068531
nic <nrhill1@gmail.com>
2024-02-24 16:20:08 -0800

    adding some background color

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,14 +7,17 @@ export default function Home() {
 
   const socket = io('ws://0.0.0.0:8080/');
 
-  const [message, setMessage] = useState('');
 
+  const [message, setMessage] = useState('');
+  const [socketId, setSocketId] = useState('');
   useEffect(() => {
     socket.connect();
 
     socket.on('connect', () => {
       console.log('socket connected!');
     });
+
+    setSocketId(socket.id?.toString() || '');
 
     socket.on('message-back', (data: any) => {
       console.log('message back: ', data);
@@ -24,10 +27,21 @@ export default function Home() {
 
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>{message}</p>
-      <button onClick={() => socket.send('message', 'Hello')}>Send</button>
+    <div className="grid container p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg items-center space-x-4 mt-2">
+      {/* <p>Hello user{socketId}!</p> */}
+      <p className="">{message}</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => socket.send('message', 'Hello')}
+      >
+        Send
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setMessage('')}
+      >
+          Clear
+      </button>
     </div>
   )
 
